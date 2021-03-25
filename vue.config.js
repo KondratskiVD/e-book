5f9b1af7ae00816decebec78c05db8d9171336a1
Kondratski Vitaliy <kondratskivd@gmail.com>
2021-03-25 21:57:39 +0200

    remove vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,3 @@
-const  HtmlWebpackInlineSourcePlugin  = require('html-webpack-inline-source-plugin')
-const  HtmlWebpackPlugin  = require('html-webpack-plugin')
-const path = require('path')
-
 module.exports = {
-  lintOnSave: false,
-  publicPath: '/',
-  css: {
-    extract: false
-  },
-  configureWebpack: {
-    optimization: {
-      splitChunks: false
-    },
-    resolve: {
-      extensions: ['*', '.js'],
-      modules: [path.resolve(__dirname, 'node_modules')]
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        inlineSource: '.(js|css)$' // embed all javascript and css inline
-      }),
-      new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin)
-    ]
-  }
+  publicPath: '/'
 }
