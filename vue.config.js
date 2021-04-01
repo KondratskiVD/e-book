9ebc72ba39a85dbd5001ab16c3baa485d5c4ffe4
Kondratski Vitaliy <kondratskivd@gmail.com>
2021-04-01 16:25:02 +0300

    changed file name of builded files

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,19 @@
-const webpack = require('webpack');
+const webpack = require('webpack')
+const path = require('path')
 
 module.exports = {
   publicPath: '/',
   filenameHashing: false,
   productionSourceMap: false,
+  css: {
+    extract: {
+      filename: 'widget.css'
+    }
+  },
   configureWebpack: {
+    output: {
+      filename: 'widget.js'
+    },
     plugins: [
       new webpack.optimize.LimitChunkCountPlugin({
         maxChunks: 1
